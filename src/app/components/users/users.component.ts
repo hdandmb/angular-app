6c0804e820b02e945cd261d2930b8a64fdbfa219
Mia Boyles <hdandmb@gmail.com>
2018-02-20 13:39:43 -0800

    added an ngFor loop to loop through the user object

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+
+import {
+  User
+} from '../../models/User';
 
 @Component({
   selector: 'app-users',
@@ -6,11 +13,57 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
-
-  constructor() { }
+  users: User[];
+  constructor() {}
 
   ngOnInit() {
-    console.log('hi');
+    this.users = [
+      {
+        firstName: 'John',
+        lastName: 'Doe',
+        age: 30,
+        address: {
+          street: '476 Yale ave',
+          city: 'Spring Field',
+          state: 'Co'
+        }
+      },
+      {
+        firstName: 'Mary',
+        lastName: 'Jameson',
+        age: 56,
+        address: {
+          street: '31 Cole st',
+          city: 'New York',
+          state: 'NY'
+        }
+      },
+      {
+        firstName: 'Latesha',
+        lastName: 'Banks',
+        age: 22,
+        address: {
+          street: '4019 Kakashi st',
+          city: 'Denver',
+          state: 'Co'
+        }
+      }
+    ];
+
+    this.addUser({
+      firstName: 'Ren',
+      lastName: 'Nates',
+      age: 23,
+      address: {
+        street: '39 Main st',
+        city: 'Boise',
+        state: 'Idaho'
+      }
+    });
   }
 
+
+  addUser(user: User) {
+    this.users.push(user);
+  }
 }
