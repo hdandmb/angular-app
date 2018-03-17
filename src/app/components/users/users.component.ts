80a423db4125bdbce12389caa313c8a1aa4b0c85
Mia Boyles <hdandmb@gmail.com>
2018-03-16 22:14:03 -0700

    added pipes

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -15,7 +15,7 @@ import {
 
 export class UsersComponent implements OnInit {
   users: User[];
-  showExtended = false;
+  showExtended true;
   loaded = false;
   buttonAddNewUser = true;
   currentClasses: {};
@@ -38,7 +38,9 @@ export class UsersComponent implements OnInit {
           state: 'Co'
         },
         image: 'http://lorempixel.com/600/600/people/5',
-        isActive: true
+        isActive: true,
+        balance: 100,
+        registered: new Date('06/03/2018 08:12:02' )
       },
       {
         firstName: 'Mary',
@@ -50,7 +52,9 @@ export class UsersComponent implements OnInit {
           state: 'NY'
         },
         image: 'http://lorempixel.com/600/600/people/8',
-        isActive: true
+        isActive: true,
+        balance: 168,
+        registered: new Date('12/28/2016 03:16:18' )
       },
       {
         firstName: 'Latesha',
@@ -62,7 +66,9 @@ export class UsersComponent implements OnInit {
           state: 'Co'
         },
         image: 'http://lorempixel.com/600/600/people/7',
-        isActive: false
+        isActive: false,
+        balance: 698,
+        registered: new Date('07/02/2017 06:23:31' )
       }
     ];
 
